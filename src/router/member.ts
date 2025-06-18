bb39638fdb26bc1486c0cec49f13bfeeb328b79b
FATIKAHMED <fatik.khan.200@gmail.com>
2025-06-18 16:56:13 +0500

    authenticate middleware added

--- src/router/member.ts
+++ src/router/member.ts
@@ -2,12 +2,15 @@ import { Router } from "express";
 import {  memberController } from "../controllers";
 import { asyncHandler } from "../util/async-handles";
 import PathsV1 from "./paths";
+import { AuthenticateMW } from "../middleware";
 
 const memberRouter: Router = Router({ mergeParams: true });
 
-//? @api  = /api/signup
-//? @desc = Register a new user
+//? @api  = /api/add-loyal-member
+//? @desc = Register a new member via API
 memberRouter.post(PathsV1.Member.loyalMemberAdd, asyncHandler(memberController.addMemberViaApi));
-memberRouter.post(PathsV1.Member.add, asyncHandler(memberController.addMember));
+//? @api  = /api/add-member
+//? @desc = Register a new member
+memberRouter.post(PathsV1.Member.add,asyncHandler(AuthenticateMW), asyncHandler(memberController.addMember));
 
 export { memberRouter };
